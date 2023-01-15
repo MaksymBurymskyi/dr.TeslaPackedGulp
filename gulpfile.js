f9e8c3d60955861c75ad82469bac03ec33bd2058
Maksym84 <burimskiy84@gmail.com>
2023-01-15 11:40:57 +0200

    collected for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,54 @@
-'use strict'; 
+"use strict";
 
-let gulp = require('gulp'),
-    sass = require('gulp-sass')(require('sass')),
+let gulp = require("gulp"),
+    sass = require("gulp-sass")(require("sass")),
     autoprefixer = require("gulp-autoprefixer"),
-    sourcemaps = require('gulp-sourcemaps'),
+    sourcemaps = require("gulp-sourcemaps"),
     rename = require("gulp-rename"),
-    browserSync = require("browser-sync").create();;
+    browserSync = require("browser-sync").create();
 
 gulp.task("sass", function () {
     return gulp
         .src("./scss/*.scss")
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: "compressed"
-        }).on("error", sass.logError))
+        .pipe(
+            sass({
+                outputStyle: "compressed",
+            }).on("error", sass.logError)
+        )
         .pipe(
             autoprefixer({
-                cascade: false
+                cascade: false,
             })
         )
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'))
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
 gulp.task(
-    'serve',
-    gulp.series('sass', function () {
+    "serve",
+    gulp.series("sass", function () {
         browserSync.init({
-            server: './'
+            server: "./",
         });
 
         gulp.watch("./scss/**/*.scss", gulp.parallel("sass"));
     })
 );
 
-gulp.task('clean', function () {
-    return gulp.src('./css', {
-        allowEmpty: true
-    }).pipe(clean())
-})
+gulp.task("clean", function () {
+    return gulp
+        .src("./css", {
+            allowEmpty: true,
+        })
+        .pipe(clean());
+});
 
-gulp.task('default', gulp.series('serve'));
+gulp.task("default", gulp.series("serve"));
